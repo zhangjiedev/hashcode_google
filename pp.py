0a572027f9319c9211d20f488e2793dad9d6ad33
jack <jack@dhcp-v115-057.mobile.uci.edu>
2020-02-15 16:21:41 -0800

    first commit

---
+++ pp.py
@@ -0,0 +1,12 @@
+import numpy as np
+def getTest(filename: str):
+    f = open('data/'+ filename, 'r')
+    first = f.readline()
+    second = f.readline()
+    f.close()
+    M, N = first.split()
+    M, N = int(M), int(N)
+    S = np.array([int(i) for i in second.split()])
+    return M, N, S
+
+print(getTest('a_example.in'))
